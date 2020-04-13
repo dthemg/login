4b565b248915bf6c1224b22fc62f2a7697d574f2
dthemg <david.montgomery@live.se>
2020-04-13 13:15:35 +0200

    Setting up frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      "username": "", 
+      "password": ""
+    }
+    this.onLoginAttempt = this.onLoginAttempt.bind(this);
+    this.renderLoginFields = this.renderLoginFields.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+  
+  onLoginAttempt() {
+    console.log("Attempting to log in...")
+  }
+
+  onChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  renderLoginFields() {
+    return (
+      <div>
+        <div>
+            <TextField
+              label="Username"
+              name="username"
+              value={ this.state.username }
+              onChange={ this.onChange }
+            >
+            </TextField>
+        </div>
+        <div>
+            <TextField
+              label="Password"
+              name="password"
+              value={ this.state.password }
+              onChange={ this.onChange }
+              >
+              </TextField>
+        </div>
+        <div>
+          <Button
+            onClick={ this.onLoginAttempt }
+            variant="contained"
+          >
+            Login
+          </Button>
+        </div>
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Login starts here</h1>
+        { this.renderLoginFields() }
+      </div>
+    )
+  }
+
 }
+
 
 export default App;
